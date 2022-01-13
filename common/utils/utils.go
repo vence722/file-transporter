bfc226d6e85600872008a8ddaa2df2c5b806d876
Vence Lam <vence722@users.noreply.github.com>
2022-01-13 20:12:24 +0800

    change line seperator logic

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -3,11 +3,19 @@ package utils
 import (
 	"bufio"
 	"os"
+	"runtime"
 	"strings"
 )
 
 func ReadCliInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	return strings.ReplaceAll(text, "\n", "")
+	return strings.ReplaceAll(text, GetLineSeparator(), "")
+}
+
+func GetLineSeparator() string {
+	if runtime.GOOS == "windows" {
+		return "\r\n"
+	}
+	return "\n"
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module file-transporter
 
 go 1.18
 
-require github.com/vence722/convert v0.0.0-20200430085311-f6b1542efdae // indirect
+require github.com/vence722/convert v0.0.0-20200430085311-f6b1542efdae
